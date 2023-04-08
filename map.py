a1e28f872bbd8d33a3befbd0dbf5f1e050e1036b
Itay <itay3742@gmail.com>
2023-04-08 22:57:47 +0300

    Update map.py

--- map.py
+++ map.py
@@ -13,3 +13,12 @@ mini_map = [
     [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
     ]
 
+class Map:
+    def __init__(self,game):
+        self.game = game
+        self.mini_map = mini_map
+        self.world_map = {}
+        self.get_map()
+
+    def get_map(self):
+        pass
