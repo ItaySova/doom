08ed76ed0c278d0cf43ba427074cf25e645094fe
Itay <itay3742@gmail.com>
2023-04-09 21:42:15 +0300

    Update player.py

--- player.py
+++ player.py
@@ -1,3 +1,12 @@
 import pygame as pg
 from settings import *
 import math
+
+class Player:
+    def __init__(self, game):
+        self.game = game
+        self.x, self.y = PLAYER_POS
+        self.angle = PLAYER_ANGLE
+
+    def movement(self):
+        pass
