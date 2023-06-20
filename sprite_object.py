0316dd3af6c9f496c564883ad36a7e5b4e05e206
Itay <itay3742@gmail.com>
2023-06-20 15:37:56 +0300

    Update sprite_object.py

--- sprite_object.py
+++ sprite_object.py
@@ -8,4 +8,10 @@ class SpriteObject:
         self.x, self.y = pos
         self.image = pg.image.load(path).convert_alpha()
         self.IMAGE_WIDTH = self.image.get_width()
-        self.IMAGE_HALF_WIDTH =  self.image.get_width() // 2
+        self.IMAGE_HALF_WIDTH = self.image.get_width() // 2
+
+    def get_sprite(self):
+        pass
+
+    def update(self):
+        self.get_sprite()
