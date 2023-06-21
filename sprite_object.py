cc68f5ea32fa28d2a1bde1595e03664815ef0434
Itay <itay3742@gmail.com>
2023-06-21 19:55:16 +0300

    Update sprite_object.py

--- sprite_object.py
+++ sprite_object.py
@@ -11,7 +11,17 @@ class SpriteObject:
         self.IMAGE_HALF_WIDTH = self.image.get_width() // 2
 
     def get_sprite(self):
-        pass
+        dx = self.x - self.player.x
+        dy = self.y - self.player.y
+        self.dx, self.dy = dx, dy
+        self.theta = math.atan2(dy, dx)
+
+        delta = self.theta - self.player.angle
+        if (dx > 0 and self.player.angle > math.pi) or (dx < 0 and dy < 0):
+            delta += math.tau
+
+        delta_rays = delta / DELTA_ANGLE
+        self.screen_x = (HALF_NUM_RAYS + delta_rays) * SCALE
 
     def update(self):
         self.get_sprite()
