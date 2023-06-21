1940e186d84f2500a44ce7015063880ed9cb0dff
Itay <itay3742@gmail.com>
2023-06-21 20:19:47 +0300

    Update sprite_object.py

--- sprite_object.py
+++ sprite_object.py
@@ -23,5 +23,8 @@ class SpriteObject:
         delta_rays = delta / DELTA_ANGLE
         self.screen_x = (HALF_NUM_RAYS + delta_rays) * SCALE
 
+        self.dist = math.hypot(dx, dy)
+        self.norm_dist = self.dist * math.cos(delta)
+
     def update(self):
         self.get_sprite()
