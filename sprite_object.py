3a65e342c49693ef4da0bd5c94bbc882dea0ef16
Itay <itay3742@gmail.com>
2023-06-23 19:38:22 +0300

    Update sprite_object.py

--- sprite_object.py
+++ sprite_object.py
@@ -63,7 +63,11 @@ class AnimatedSprite(SpriteObject):
         self.animation_trigger = False
 
     def check_animation_time(self):
-        pass
+        self.animation_trigger = False
+        time_now = pg.time.get_ticks()
+        if time_now - self.animation_time_prev > self.animation_time:
+            self.animation_time_prev = time_now
+            self.animation_trigger = True
 
     def get_images(self, path):
         images = deque()
