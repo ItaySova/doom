bab5b81c11329384ec9c3b592169e3d4ec74a576
Itay <itay3742@gmail.com>
2023-06-24 19:34:05 +0300

    Create Weapon.py

---
+++ Weapon.py
@@ -0,0 +1,14 @@
+
+
+class Weapon:
+    def __init__(self, game):
+        self.game = game
+
+    def animate_shot(self):
+        pass
+
+    def draw(self):
+        pass
+
+    def update(self):
+        pass
