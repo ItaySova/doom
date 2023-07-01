410b7cca593f4aaa621e522a9e78e24bb3d83afe
Itay <itay3742@gmail.com>
2023-07-01 14:43:07 +0300

    Update pathfinding.py

--- pathfinding.py
+++ pathfinding.py
@@ -9,6 +9,12 @@ class PathFinding:
         self.graph = {}
         self.get_graph()
 
+    def get_path(self, start, goal):
+        pass
+
+    def bfs(self, start, goal, graph):
+        pass
+
     def get_next_nodes(self, x, y):
         return [(x + dx, y + dy) for dx, dy in self.ways if (x + dx, y + dy) not in self.game.map.world_map]
 
