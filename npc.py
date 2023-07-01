f6c2e9d4afe02215d49bfba2c7c7e977322fbdcc
Itay <itay3742@gmail.com>
2023-07-01 13:31:29 +0300

    Update npc.py

--- npc.py
+++ npc.py
@@ -6,7 +6,7 @@ class NPC(AnimatedSprite):
     def __init__(self, game, path='resources/sprites/npc/soldier/0.png', pos=(10.5,5.5),
                  scale=0.6, shift=0.38, animation_time=180):
         super().__init__(game, path, pos, scale, shift, animation_time)
-        self.attack_images = self.get_images(path + '/attack')
+        self.attack_images = self.get_images(self.path + '/attack')
         self.death_images = self.get_images(self.path + '/death')
         self.idle_images = self.get_images(self.path + '/idle')
         self.pain_images = self.get_images(self.path + '/pain')
