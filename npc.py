f63e20ca5266ad40c706e8bed5b3f64b6adebb4d
Itay <itay3742@gmail.com>
2023-07-01 14:03:48 +0300

    Update npc.py

--- npc.py
+++ npc.py
@@ -21,6 +21,7 @@ class NPC(AnimatedSprite):
         self.alive = True
         self.pain = False
         self.ray_cast_value = False
+        self.frame_counter = 0
 
     def update(self):
         self.check_animation_time()
@@ -33,6 +34,13 @@ class NPC(AnimatedSprite):
         if self.animation_trigger:
             self.pain = False
 
+    def animate_death(self):
+        if not self.alive:
+            if self.animation_trigger and self.frame_counter < len(self.death_images) - 1:
+                self.death_images.rotate(-1)
+                self.image = self.death_images[0]
+                self.frame_counter += 1
+
     def check_hit_in_npc(self):
         if self.ray_cast_value and self.game.player.shot:
             if HALF_WIDTH - self.sprite_half_width < self.screen_x < HALF_WIDTH + self.sprite_half_width:
@@ -43,7 +51,9 @@ class NPC(AnimatedSprite):
                 self.check_health()
 
     def check_health(self):
-        pass
+        if self.health < 1:
+            self.alive = False
+            self.game.sound.npc_death.play()
 
     def run_logic(self):
         if self.alive:
@@ -53,6 +63,8 @@ class NPC(AnimatedSprite):
                 self.animate_pain()
             else:
                 self.animate(self.idle_images)
+        else:
+            self.animate_death()
 
     @property
     def map_pos(self):
